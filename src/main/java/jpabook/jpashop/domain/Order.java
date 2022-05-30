a2c017f1c7f677da8a2faa7f48a01b1235b2c91a
INFI\Administrator <gygus7345@naver.com>
2022-05-30 15:24:30 +0900

    주문 서비스로직 구현

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.jms.JmsProperties;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -38,5 +39,44 @@ public class Order {
         this.member = member;
     }
 
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivary delivery){
+        this.delivary = delivery;
+        delivery.setOrder(this);
+    }
+
+    public static Order createOrder(Member member, Delivary delivary, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivary);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    public void cancel(){
+        if(delivary.getStatus() == DelivaryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 
 }
